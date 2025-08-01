a717c10eb8d8e7536a0e8939c67f31f3b71937ac
Minwanabi <lundjonas@hotmail.com>
2025-08-01 21:28:58 +0200

    IT WORKS! NOW I NEED MORE BOOKS TO ANALYZE

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
+import sys
 from stats import count_words
-from stats import get_book_text
+
 from stats import count_letters
+from stats import sort_dictionary
+
 
 def main():
-    file_contents = get_book_text("books/frankenstein.txt")
-    print("File contents NOT printed")
-    word_count = count_words("books/frankenstein.txt")
-    print(f"{word_count} words found in the document.")
-    letter_count = count_letters("books/frankenstein.txt")
-    
+    print(sys.argv)
+    print(len(sys.argv))
+    if len(sys.argv) <= 1:
+        print("No input file provided. Use the path to the book you want to analyze.")
+        sys.exit(1)
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book {sys.argv[1]}...")
+    print("----------- Word Count ----------")
+    word_count = count_words(sys.argv[1])
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    letter_count = count_letters(sys.argv[1])
+    sorted_dict_desc = dict(sorted(letter_count.items(), key=lambda item: item[1], reverse=True))
+    for i in sorted_dict_desc:
+        print(f"{i}: {sorted_dict_desc[i]}")
+
+    print("============= END ===============")
+
 
 main()
+
--- stats.py
+++ stats.py
@@ -1,16 +1,32 @@
+import sys
+
+input = sys.argv[1]
+
 def get_book_text(input):
+
     with open(input) as f:
         file_contents = f.read()
     return file_contents
 
 def count_words(input):
+
     file_contents = get_book_text(input)
     words = file_contents.split()
     return len(words)
 
 def count_letters(input):
+
     with open(input) as f:
-        file_contents = f.read()
-        print(type(file_contents))  # Debugging line
-        print("file_contents[0:100] =", file_contents[0:100])  # Debugging line
-       
+        file_contents = f.read() # read the file contents and saves as a string
+        file_contents = file_contents.lower() # set to lowercase for uniformity
+        dictionary = {}
+        for i in file_contents:
+            if i in dictionary:
+                dictionary[i] += 1
+            else:
+                dictionary[i] = 1
+    return dictionary
+
+def sort_dictionary(count_letters):
+    sorted(count_letters)           
+    return sorted_dict    
